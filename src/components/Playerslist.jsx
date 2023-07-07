4328414168e4b4d2159ed04d1a1774974952ac40
unknown <eunzyfads@gmail.com>
2023-07-07 17:06:46 +0100

    added new project

---
+++ src/components/Playerslist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Player from "./Player";
+import players from "../players";
+
+const PlayersList = () => {
+  return (
+    <div style={{display:'grid', gridTemplateColumns:"1fr 1fr 1fr"}}>
+      {players.map((players, index) => (
+        <Player key={index} {...players} />
+      ))}
+    </div>
+  );
+};
+
+export default PlayersList;
